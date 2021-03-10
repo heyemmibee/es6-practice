b78ec6ad13234ec5e884e972c047c30f5e7126d7
heyemmibee <eburnaman@gmail.com>
2021-03-10 10:32:07 -0500

    bad connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
-import "./styles.css";
+// ///////
+// const squares = [1,2,3,4,5,6,7,8,9,10].map(square => square * square);
+// console.log(...squares);
+// //////
 
-const squares = [1,2,3,4,5,6,7,8,9,10].map(square => square * square);
-console.log(...squares);
+function createSundae({scoops = 1, toppings = ['Hot Fudge']} = {}) {
+    const scoopText = scoops === 1 ? 'scoop' : 'scoops';
+    return `Your sundae has ${scoops} ${scoopText} with ${toppings.join(' and ')} toppings.`;
+  }
+console.log (createSundae);
+//////
