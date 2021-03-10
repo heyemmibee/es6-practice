e5aa76c8a1f343faa2a58b9010830e4bd5ecbf05
heyemmibee <eburnaman@gmail.com>
2021-03-10 12:29:03 -0500

    mapping arrays

--- src/app.js
+++ src/app.js
@@ -11,32 +11,82 @@
 // console.log (createSundae);
 
 //////
-function houseDescriptor({houseColor = 'green', shutterColors = ['red']} = {}) {
-    return `I have a ${houseColor} house with ${shutterColors.join(' and ')} shutters`;
+// function houseDescriptor({houseColor = 'green', shutterColors = ['red']} = {}) {
+//     return `I have a ${houseColor} house with ${shutterColors.join(' and ')} shutters`;
+//   }
+  
+//   houseDescriptor({houseColor:'red',shutterColors:['white','gray','pink']});
+
+// //////
+// function buildHouse({floors = 1, color = 'red', walls='brick'} = {}) {
+//     return `Your house has ${floors} floor(s) with ${color} ${walls} walls.`;
+// }
+// console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+// console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+// console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
+
+
+// ////// javascript classes & class methods
+// class Snowpiercer {
+//     constructor(numEngine) {
+//         this.numEngines = numEngine;
+//         this.enginesActive = false;
+//     }
+//     startEngine() {
+//         console.log('start your engine...1001 cars long');
+//         this.enginesActive = true;
+//     }
+// }
+
+// let emilysTrain = new Snowpiercer(1);
+// emilysTrain.startEngine();
+
+
+
+function Tree(size, leaves) {
+    this.size = (typeof size === "undefined")? 10 : size;
+    const defaultLeaves = {spring: 'green', summer: 'green', fall: 'orange', winter: null};
+    this.leaves = (typeof leaves === "undefined")?  defaultLeaves : leaves;
+    this.leafColor;
   }
   
-  houseDescriptor({houseColor:'red',shutterColors:['white','gray','pink']});
-
-//////
-function buildHouse({floors = 1, color = 'red', walls='brick'} = {}) {
-    return `Your house has ${floors} floor(s) with ${color} ${walls} walls.`;
-}
-console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
-console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
-console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
-
-
-////// javascript classes & class methods
-class Snowpiercer {
-    constructor(numEngine) {
-        this.numEngines = numEngine;
-        this.enginesActive = false;
+  Tree.prototype.changeSeason = function(season) {
+    this.leafColor = this.leaves[season];
+    if (season === 'spring') {
+      this.size += 1;
     }
-    startEngine() {
-        console.log('start your engine...1001 cars long');
-        this.enginesActive = true;
+  }
+  
+  function Maple (syrupQty, size, leaves) {
+    Tree.call(this, size, leaves);
+    this.syrupQty = (typeof syrupQty === "undefined")? 15 : syrupQty;
+  }
+  
+  Maple.prototype = Object.create(Tree.prototype);
+  Maple.prototype.constructor = Maple;
+  
+  Maple.prototype.changeSeason = function(season) {
+    Tree.prototype.changeSeason.call(this, season);
+    if (season === 'spring') {
+      this.syrupQty += 1;
     }
-}
+  }
+  
+  Maple.prototype.gatherSyrup = function() {
+    this.syrupQty -= 3;
+  }
+  
+  const myMaple = new Maple(15, 5);
+  myMaple.changeSeason('fall');
+  myMaple.gatherSyrup();
+  myMaple.changeSeason('spring');
 
-let emilysTrain = new Snowpiercer(1);
-emilysTrain.startEngine();
+
+
+class Apple {}
+class GrannySmith extends Apple {
+  constructor(tartnessLevel, energy) {
+    this.tartnessLevel = tartnessLevel; // `this` before `super` will throw an error!
+    super(energy); 
+  }
+}
